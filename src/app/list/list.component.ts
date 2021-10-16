b214443cb241ad161f246e58973e2bfd3b071fbd
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-16 21:49:24 +0300

    Add some logic

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { switchMap, map, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
+
 export class ListComponent implements OnInit {
+  private items$: Subject<DataList>;
+  public dataArray: Array<any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.dataArray = [];
+    this.items$ = new Subject<DataList>();
   }
 
+  ngOnInit() {
+    this.items$.subscribe(value => this.dataArray.push(value));
+  }
+
+  public addData() {
+    this.items$.next({value: 'HELLO', date: new Date});
+    console.log('clicked');
+  }
+}
+
+interface DataList {
+  value: string,
+  date: Date,
 }
