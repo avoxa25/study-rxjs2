b60967667bd8d14a2818056b7293fc98ffeaec59
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-17 03:57:35 +0300

    Finish popup

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../mock';
+import { PopupService } from '../popup.service';
 
 @Component({
   selector: 'app-popup',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent implements OnInit {
+  public isHidden: boolean;
+  public product?: IProduct;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private popupService: PopupService) {
+    this.isHidden = true;
   }
 
+  ngOnInit() {
+    this.popupService.getStream().subscribe(data => {
+      this.product = data;
+      this.isHidden = false
+    });
+  }
+
+  public close(): void {
+    this.isHidden = true;
+  }
 }
